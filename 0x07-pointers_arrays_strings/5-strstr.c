1ab0d1e1c4f41c450d3232fd41023d81ceb2b6cc
prins-ebuka <ecihekwereme@gmail.com>
2023-01-02 17:38:07 -0800

    alx 0x07-pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "my_function.h"
+#include <stdio.h>
+/**
+ * _strstr - function that locates a substring
+ * @haystack: initial string
+ * @needle: sbstring
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *res;
+	char *aux;
+
+	while (*haystack != '\0')
+	{
+		res = haystack;
+		aux = needle;
+		while (*aux == *haystack && *aux != '\0' && *haystack != '\0')
+		{
+			haystack++;
+			aux++;
+		}
+		if (*aux == '\0')
+			return (res);
+		haystack = res + 1;
+	}
+	return (NULL);
+}
